e21d5cba95af11c136c823d611a52d85c020e167
Travis <travis@travis-ci.org>
2015-04-17 16:52:39 -0400

    Add new methods

--- Receiver/Miscellaneous/Meta.php
+++ Receiver/Miscellaneous/Meta.php
@@ -1,6 +1,19 @@
 <?php
 namespace Scion\GitHub\Receiver\Miscellaneous;
 
+/**
+ * The Meta API class provides information about GitHub.com (the service) or your organization’s GitHub Enterprise installation.
+ * @see     https://developer.github.com/v3/meta/
+ * @package Scion\GitHub\Receiver\Miscellaneous
+ */
 class Meta extends AbstractMiscellaneous {
 
+	/**
+	 * Meta, provides information about GitHub.com, the service.
+	 * @link https://developer.github.com/v3/meta/#meta
+	 * @return string
+	 */
+	public function get() {
+		return $this->getApi()->request('/meta');
+	}
 } 
