dcb5988e87242814a28ddc2fd7e8863e29f5da23
David Sanchez <travis@travis-ci.org>
2015-04-21 15:49:27 -0400

    Update methods

--- Receiver/Repositories/DeployKeys.php
+++ Receiver/Repositories/DeployKeys.php
@@ -3,54 +3,63 @@ namespace Scion\GitHub\Receiver\Repositories;
 
 use Scion\Http\Request;
 
+/**
+ * The DeployKeys API class provides access to repository's deploy keys.
+ * @link    https://developer.github.com/v3/repos/keys/
+ * @package Scion\GitHub\Receiver\Repositories
+ */
 class DeployKeys extends AbstractRepositories {
 
 	/**
 	 * List deploy keys
-	 * @see https://developer.github.com/v3/repos/keys/#list
+	 * @link https://developer.github.com/v3/repos/keys/#list
 	 * @return mixed
 	 */
 	public function listDeployKeys() {
 		return $this->getApi()->request(
-			sprintf('/repos/%s/%s/keys', $this->getRepositories()->getOwner(), $this->getRepositories()->getRepo())
+			$this->getApi()->getString()->sprintf('/repos/:owner/:repo/keys', $this->getRepositories()->getOwner(), $this->getRepositories()->getRepo())
 		);
 	}
 
 	/**
 	 * Get a deploy key
-	 * @see https://developer.github.com/v3/repos/keys/#get
+	 * @link https://developer.github.com/v3/repos/keys/#get
 	 * @param int $id
 	 * @return mixed
 	 */
 	public function getDeployKey($id) {
 		return $this->getApi()->request(
-			sprintf('/repos/%s/%s/keys/%s', $this->getRepositories()->getOwner(), $this->getRepositories()->getRepo(), $id)
+			$this->getApi()->getString()->sprintf('/repos/:owner/:repo/keys/:id', $this->getRepositories()->getOwner(), $this->getRepositories()->getRepo(), $id)
 		);
 	}
 
 	/**
 	 * Add a new deploy key
-	 * @see https://developer.github.com/v3/repos/keys/#create
+	 * @link https://developer.github.com/v3/repos/keys/#create
 	 * @param string $title
 	 * @param string $key
 	 * @return mixed
 	 */
 	public function addNewDeployKey($title, $key) {
 		return $this->getApi()->request(
-			sprintf('/repos/%s/%s/keys?title=%s&key=%s', $this->getRepositories()->getOwner(), $this->getRepositories()->getRepo(), $title, $key),
-			Request::METHOD_POST
+			$this->getApi()->getString()->sprintf('/repos/:owner/:repo/keys', $this->getRepositories()->getOwner(), $this->getRepositories()->getRepo()),
+			Request::METHOD_POST,
+			[
+				'title' => $title,
+				'key'   => $key
+			]
 		);
 	}
 
 	/**
 	 * Remove a deploy key
-	 * @see https://developer.github.com/v3/repos/keys/#delete
+	 * @link https://developer.github.com/v3/repos/keys/#delete
 	 * @param int $id
 	 * @return mixed
 	 */
 	public function removeDeployKey($id) {
 		return $this->getApi()->request(
-			sprintf('/repos/%s/%s/keys/%s', $this->getRepositories()->getOwner(), $this->getRepositories()->getRepo(), $id),
+			$this->getApi()->getString()->sprintf('/repos/:owner/:repo/keys/:id', $this->getRepositories()->getOwner(), $this->getRepositories()->getRepo(), $id),
 			Request::METHOD_DELETE
 		);
 	}
