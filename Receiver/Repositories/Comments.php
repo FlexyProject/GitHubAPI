27ccf1b12ae02a922d3e6b09fe5f3343a9bd2603
David Sanchez <travis@travis-ci.org>
2015-04-21 10:57:27 -0400

    Update methods

--- Receiver/Repositories/Comments.php
+++ Receiver/Repositories/Comments.php
@@ -2,36 +2,40 @@
 namespace Scion\GitHub\Receiver\Repositories;
 
 use Scion\Http\Request;
-use Scion\GitHub\AbstractApi;
 
+/**
+ * The Comments API class provides access to repository's comments.
+ * @link    https://developer.github.com/v3/repos/comments/
+ * @package Scion\GitHub\Receiver\Repositories
+ */
 class Comments extends AbstractRepositories {
 
 	/**
 	 * List commit comments for a repository
-	 * @see https://developer.github.com/v3/repos/comments/#list-commit-comments-for-a-repository
+	 * @link https://developer.github.com/v3/repos/comments/#list-commit-comments-for-a-repository
 	 * @return mixed
 	 */
 	public function listComments() {
 		return $this->getApi()->request(
-			sprintf('/repos/%s/%s/comments', $this->getRepositories()->getOwner(), $this->getRepositories()->getRepo())
+			$this->getApi()->getString()->sprintf('/repos/:owner/:repo/comments', $this->getRepositories()->getOwner(), $this->getRepositories()->getRepo())
 		);
 	}
 
 	/**
 	 * List comments for a single commit
-	 * @see https://developer.github.com/v3/repos/comments/#list-comments-for-a-single-commit
-	 * @param $ref
+	 * @link https://developer.github.com/v3/repos/comments/#list-comments-for-a-single-commit
+	 * @param string $ref
 	 * @return mixed
 	 */
-	public function listCommitComments($ref = AbstractApi::BRANCH_MASTER) {
+	public function listCommitComments($ref) {
 		return $this->getApi()->request(
-			sprintf('/repos/%s/%s/commits/%s/comments', $this->getRepositories()->getOwner(), $this->getRepositories()->getRepo(), $ref)
+			$this->getApi()->getString()->sprintf('/repos/:owner/:repo/commits/:ref/comments', $this->getRepositories()->getOwner(), $this->getRepositories()->getRepo(), $ref)
 		);
 	}
 
 	/**
 	 * Create a commit comment
-	 * @see https://developer.github.com/v3/repos/comments/#create-a-commit-comment
+	 * @link https://developer.github.com/v3/repos/comments/#create-a-commit-comment
 	 * @param string $sha
 	 * @param string $body
 	 * @param string $path
@@ -40,45 +44,55 @@ class Comments extends AbstractRepositories {
 	 */
 	public function addCommitComment($sha, $body, $path = '', $position = 0) {
 		return $this->getApi()->request(
-			sprintf('/repos/%s/%s/commits/%s/comments?body=%s&path=%s&position=%s', $this->getRepositories()->getOwner(), $this->getRepositories()->getRepo(), $sha, $body, $path, $position),
-			Request::METHOD_POST
+			$this->getApi()->getString()->sprintf('/repos/:owner/:repo/commits/:sha/comments', $this->getRepositories()->getOwner(), $this->getRepositories()->getRepo(), $sha),
+			Request::METHOD_POST,
+			[
+				'body'     => $body,
+				'path'     => $path,
+				'position' => $position
+			]
 		);
 	}
 
 	/**
 	 * Get a single commit comment
-	 * @see https://developer.github.com/v3/repos/comments/#get-a-single-commit-comment
+	 * @link https://developer.github.com/v3/repos/comments/#get-a-single-commit-comment
 	 * @param int $id
 	 * @return mixed
 	 */
 	public function getCommitComment($id) {
 		return $this->getApi()->request(
-			sprintf('/repos/%s/%s/comments/%s', $this->getRepositories()->getOwner(), $this->getRepositories()->getRepo(), $id)
+			$this->getApi()->getString()->sprintf('/repos/:owner/:repo/comments/:id', $this->getRepositories()->getOwner(), $this->getRepositories()->getRepo(), (string)$id)
 		);
 	}
 
 	/**
 	 * Update a commit comment
-	 * @see https://developer.github.com/v3/repos/comments/#update-a-commit-comment
-	 * @param int $id
+	 * @link https://developer.github.com/v3/repos/comments/#update-a-commit-comment
+	 * @param int    $id
+	 * @param string $body
 	 * @return mixed
+	 * @throws \Exception
 	 */
-	public function updateCommitComment($id) {
+	public function updateCommitComment($id, $body) {
 		return $this->getApi()->request(
-			sprintf('/repos/%s/%s/comments/%s', $this->getRepositories()->getOwner(), $this->getRepositories()->getRepo(), $id),
-			Request::METHOD_PATCH
+			$this->getApi()->getString()->sprintf('/repos/:owner/:repo/comments/:id', $this->getRepositories()->getOwner(), $this->getRepositories()->getRepo(), (string)$id),
+			Request::METHOD_PATCH,
+			[
+				'body' => $body
+			]
 		);
 	}
 
 	/**
 	 * Delete a commit comment
-	 * @see https://developer.github.com/v3/repos/comments/#delete-a-commit-comment
+	 * @link https://developer.github.com/v3/repos/comments/#delete-a-commit-comment
 	 * @param int $id
 	 * @return mixed
 	 */
 	public function deleteCommitComment($id) {
 		return $this->getApi()->request(
-			sprintf('/repos/%s/%s/comments/%s', $this->getRepositories()->getOwner(), $this->getRepositories()->getRepo(), $id),
+			$this->getApi()->getString()->sprintf('/repos/:owner/:repo/comments/:id', $this->getRepositories()->getOwner(), $this->getRepositories()->getRepo(), (string)$id),
 			Request::METHOD_DELETE
 		);
 	}
