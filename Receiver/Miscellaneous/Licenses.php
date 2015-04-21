3e086255bb74dc57fbda336cdc8f8f4901064df0
David Sanchez <travis@travis-ci.org>
2015-04-21 14:57:30 -0400

    Update methods using setHeader()

--- Receiver/Miscellaneous/Licenses.php
+++ Receiver/Miscellaneous/Licenses.php
@@ -16,9 +16,7 @@ class Licenses extends AbstractMiscellaneous {
 	 * @return string
 	 */
 	public function listAllLicenses() {
-		$this->getApi()->setAccept('application/vnd.github.drax-preview+json');
-
-		return $this->getApi()->request('/licenses');
+		return $this->getApi()->setAccept('application/vnd.github.drax-preview+json')->request('/licenses');
 	}
 
 	/**
@@ -28,8 +26,6 @@ class Licenses extends AbstractMiscellaneous {
 	 * @return string
 	 */
 	public function getIndividualLicense($license) {
-		$this->getApi()->setAccept('application/vnd.github.drax-preview+json');
-
-		return $this->getApi()->request($this->getApi()->getString()->sprintf('/licenses/:license', $license));
+		return $this->getApi()->setAccept('application/vnd.github.drax-preview+json')->request($this->getApi()->getString()->sprintf('/licenses/:license', $license));
 	}
 }
