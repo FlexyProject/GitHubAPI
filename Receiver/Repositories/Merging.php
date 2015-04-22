a699ec7ada7823975f0b43df74ddf9da3b8660ef
David Sanchez <david38sanchez@gmail.com>
2015-04-21 21:01:59 -0400

    Update

--- Receiver/Repositories/Merging.php
+++ Receiver/Repositories/Merging.php
@@ -3,20 +3,30 @@ namespace Scion\GitHub\Receiver\Repositories;
 
 use Scion\Http\Request;
 
+/**
+ * The Merging API class provides access to repository's merging.
+ * @link    https://developer.github.com/v3/repos/merging/
+ * @package Scion\GitHub\Receiver\Repositories
+ */
 class Merging extends AbstractRepositories {
 
 	/**
 	 * Perform a merge
-	 * @see https://developer.github.com/v3/repos/merging/#perform-a-merge
-	 * @param string $base
-	 * @param string $head
-	 * @param string $commitMessage
+	 * @link https://developer.github.com/v3/repos/merging/#perform-a-merge
+	 * @param string      $base
+	 * @param string      $head
+	 * @param string|null $commitMessage
 	 * @return mixed
 	 */
-	public function performMerge($base, $head, $commitMessage = '') {
+	public function performMerge($base, $head, $commitMessage = null) {
 		return $this->getApi()->request(
-			sprintf('/repos/%s/%s/merges?base=%s&head=%s&commit_message=%s', $this->getRepositories()->getOwner(), $this->getRepositories()->getRepo(), $base, $head, $commitMessage),
-			Request::METHOD_POST
+			$this->getApi()->getString()->sprintf('/repos/:owner/:repo/merges', $this->getRepositories()->getOwner(), $this->getRepositories()->getRepo()),
+			Request::METHOD_POST,
+			[
+				'base'           => $base,
+				'head'           => $head,
+				'commit_message' => $commitMessage
+			]
 		);
 	}
 } 
