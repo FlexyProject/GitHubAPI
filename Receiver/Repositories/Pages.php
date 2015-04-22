247e0266226f4fa34ea54822e5dfbb1978ed0c5e
David Sanchez <travis@travis-ci.org>
2015-04-22 14:42:09 -0400

    Update methods

--- Receiver/Repositories/Pages.php
+++ Receiver/Repositories/Pages.php
@@ -1,38 +1,43 @@
 <?php
 namespace Scion\GitHub\Receiver\Repositories;
 
+/**
+ * The Pages API class provides access to repository's pages.
+ * @link    https://developer.github.com/v3/repos/pages/
+ * @package Scion\GitHub\Receiver\Repositories
+ */
 class Pages extends AbstractRepositories {
 
 	/**
 	 * Get information about a Pages site
-	 * @see https://developer.github.com/v3/repos/pages/#get-information-about-a-pages-site
+	 * @link https://developer.github.com/v3/repos/pages/#get-information-about-a-pages-site
 	 * @return mixed
 	 */
 	public function getInformation() {
 		return $this->getApi()->request(
-			sprintf('/repos/%s/%s/pages', $this->getRepositories()->getOwner(), $this->getRepositories()->getRepo())
+			$this->getApi()->getString()->sprintf('/repos/:owner/:repo/pages', $this->getRepositories()->getOwner(), $this->getRepositories()->getRepo())
 		);
 	}
 
 	/**
 	 * List Pages builds
-	 * @see https://developer.github.com/v3/repos/pages/#list-pages-builds
+	 * @link https://developer.github.com/v3/repos/pages/#list-pages-builds
 	 * @return mixed
 	 */
 	public function listPagesBuilds() {
 		return $this->getApi()->request(
-			sprintf('/repos/%s/%s/pages/builds', $this->getRepositories()->getOwner(), $this->getRepositories()->getRepo())
+			$this->getApi()->getString()->sprintf('/repos/:owner/:repo/pages/builds', $this->getRepositories()->getOwner(), $this->getRepositories()->getRepo())
 		);
 	}
 
 	/**
 	 * List latest Pages build
-	 * @see https://developer.github.com/v3/repos/pages/#list-latest-pages-build
+	 * @link https://developer.github.com/v3/repos/pages/#list-latest-pages-build
 	 * @return mixed
 	 */
 	public function listLatestPagesBuilds() {
 		return $this->getApi()->request(
-			sprintf('/repos/%s/%s/pages/builds/latest', $this->getRepositories()->getOwner(), $this->getRepositories()->getRepo())
+			$this->getApi()->getString()->sprintf('/repos/:owner/:repo/pages/builds/latest', $this->getRepositories()->getOwner(), $this->getRepositories()->getRepo())
 		);
 	}
 }
